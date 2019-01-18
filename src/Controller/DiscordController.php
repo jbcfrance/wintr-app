daf3a8b6b6bb297e9e6dda393f260061cb0d7f38
Jbcfrance <jbcfrance@gmail.com>
2019-01-18 11:06:14 +0100

    Initial commit

---
+++ src/Controller/DiscordController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class DiscordController extends AbstractController
+{
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect", name="connect")
+     */
+    public function connectAction()
+    {
+
+
+        return $this->render('login.html.twig',
+            [
+
+            ]
+        );
+    }
+
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/discord", name="connect_discord_start")
+     */
+    public function connectDiscordAction(ClientRegistry $clientRegistry)
+    {
+        // on Symfony 3.3 or lower, $clientRegistry = $this->get('knpu.oauth2.registry');
+
+        // will redirect to Discord!
+        return $clientRegistry
+            ->getClient('discord') // key used in config/packages/knpu_oauth2_client.yaml
+            ->redirect([
+                'identify', 'email', 'guilds' // the scopes you want to access
+            ])
+            ;
+    }
+
+    /**
+     * After going to Discord, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     *
+     * @Route("/connect/discord/check", name="connect_discord_check")
+     */
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        // ** if you want to *authenticate* the user, then
+        // leave this method blank and create a Guard authenticator
+        // (read below)
+
+        /** @var \KnpU\OAuth2ClientBundle\Client\Provider\DiscordClient $client */
+        $client = $clientRegistry->getClient('discord');
+
+        try {
+            // the exact class depends on which provider you're using
+            /** @var \League\OAuth2\Client\Provider\GenericProvider $user */
+            $user = $client->fetchUser();
+
+            // do something with all this new power!
+            // e.g. $name = $user->getFirstName();
+            var_dump($user); die;
+
+        } catch (IdentityProviderException $e) {
+            // something went wrong!
+            // probably you should return the reason to the user
+            var_dump($e->getMessage()); die;
+        }
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $discord_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDiscordId(): ?int
+    {
+        return $this->discord_id;
+    }
+
+    public function setDiscordId(int $discord_id): self
+    {
+        $this->discord_id = $discord_id;
+
+        return $this;
+    }
+}
